2b5e028cebb6562c565871e3e8b76a4bc32ab39e
marcinp55 <marcinp55@gmail.com>
2023-11-13 23:01:08 +0100

    Leetcode cd

--- examples/leetcode/sql50.sql
+++ examples/leetcode/sql50.sql
@@ -46,6 +46,7 @@ WHERE a1.activity_type = 'start'
 GROUP BY a1.machine_id;
 
 DROP TABLE Activity;
+DROP TYPE activity_type;
 
 -- 577. Employee Bonus --
 -- https://leetcode.com/problems/employee-bonus
@@ -86,7 +87,7 @@ WHERE b.bonus < 1000
 DROP TABLE bonus;
 DROP TABLE employee;
 
--- 1280. Students and Examinations
+-- 1280. Students and Examinations --
 -- https://leetcode.com/problems/students-and-examinations/
 CREATE TABLE IF NOT EXISTS Students
 (
@@ -148,7 +149,8 @@ SELECT sd.student_id,
        sb.subject_name,
        (SELECT COUNT(student_id)
         FROM examinations e
-        WHERE e.student_id = sd.student_id AND e.subject_name = sb.subject_name) AS attended_exams
+        WHERE e.student_id = sd.student_id
+          AND e.subject_name = sb.subject_name) AS attended_exams
 FROM subjects sb
          CROSS JOIN students sd
 ORDER BY sd.student_id, sb.subject_name;
@@ -156,3 +158,117 @@ ORDER BY sd.student_id, sb.subject_name;
 DROP TABLE subjects;
 DROP TABLE examinations;
 DROP TABLE students;
+
+-- 570. Managers with at Least 5 Direct Reports --
+-- https://leetcode.com/problems/managers-with-at-least-5-direct-reports
+CREATE TABLE IF NOT EXISTS Employee
+(
+    id         int,
+    name       varchar(255),
+    department varchar(255),
+    managerId  int
+);
+TRUNCATE TABLE Employee;
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (101, 'John', 'A', NULL);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (102, 'Dan', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (103, 'James', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (104, 'Amy', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (105, 'Anne', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (106, 'Ron', 'B', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (107, 'John', 'B', NULL);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (102, 'Dan', 'A', 107);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (103, 'James', 'A', 107);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (104, 'Amy', 'A', 107);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (105, 'Anne', 'A', 107);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (106, 'Ron', 'B', 107);
+
+
+-- Solution --
+SELECT e1.name
+FROM employee e1
+         JOIN employee e2 ON e1.id = e2.managerId
+GROUP BY e1.id, e1.name
+HAVING COUNT(*) >= 5;
+
+DROP TABLE employee;
+
+-- 1934. Confirmation Rate --
+-- https://leetcode.com/problems/confirmation-rate/
+CREATE TYPE confirmations_action AS ENUM ('confirmed', 'timeout');
+CREATE TABLE IF NOT EXISTS Signups
+(
+    user_id    int,
+    time_stamp timestamp
+);
+CREATE TABLE IF NOT EXISTS Confirmations
+(
+    user_id    int,
+    time_stamp timestamp,
+    action     confirmations_action
+);
+TRUNCATE TABLE Signups;
+INSERT INTO Signups (user_id, time_stamp)
+VALUES (3, '2020-03-21 10:16:13');
+INSERT INTO Signups (user_id, time_stamp)
+VALUES (7, '2020-01-04 13:57:59');
+INSERT INTO Signups (user_id, time_stamp)
+VALUES (2, '2020-07-29 23:09:44');
+INSERT INTO Signups (user_id, time_stamp)
+VALUES (6, '2020-12-09 10:39:37');
+TRUNCATE TABLE Confirmations;
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (3, '2021-01-06 03:30:46', 'timeout');
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (3, '2021-07-14 14:00:00', 'timeout');
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (3, '2021-07-14 14:00:00', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (7, '2021-06-12 11:57:29', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (7, '2021-06-13 12:58:28', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (7, '2021-06-14 13:59:27', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (2, '2021-01-22 00:00:00', 'confirmed');
+INSERT INTO Confirmations (user_id, time_stamp, action)
+VALUES (2, '2021-02-28 23:59:59', 'timeout');
+
+-- TODO Solution --
+WITH confirmed_count AS (SELECT s.user_id,
+                                COUNT(c.action) AS confirmed
+                         FROM signups s
+                                  LEFT JOIN confirmations c ON s.user_id = c.user_id
+                         WHERE c.action = 'confirmed'
+                         GROUP BY s.user_id),
+     total_count AS (SELECT s.user_id,
+                            COUNT(c.action) AS total
+                     FROM signups s
+                              LEFT JOIN confirmations c ON s.user_id = c.user_id
+                     GROUP BY s.user_id),
+     no_confirm_count AS (SELECT s.user_id,
+                                 0 AS confirmation_rate
+                          FROM signups s
+                                   LEFT JOIN confirmations c ON s.user_id = c.user_id
+                          WHERE c.action IS NULL)
+SELECT c.user_id,
+       CASE
+           WHEN c.confirmed IS NOT NULL THEN ROUND(CAST(c.confirmed AS numeric) / CAST(t.total AS numeric), 2)
+           ELSE 0
+           END AS confirmation_rate
+FROM confirmed_count c
+         JOIN total_count t ON c.user_id = t.user_id
+UNION ALL
+SELECT *
+FROM no_confirm_count;
